e419e4ad02e410dd0e3a3133a946ccf5d88791d4
rachidassaouii-a11y <rachid.assaouii@gmail.com>
2026-08-08 01:16:52 +0200

    Update main.py

--- main.py
+++ main.py
@@ -26,4 +26,4 @@ def ask():
     return jsonify({"response": response.text})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080))
