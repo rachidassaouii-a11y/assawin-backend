c8e201f36ac8f608ba7827feff78cb8ad108bfdf
rachidassaouii-a11y <rachid.assaouii@gmail.com>
2026-08-08 02:59:24 +0200

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ api_key = os.environ.get("GOOGLE_API_KEY")
 if api_key:
     genai.configure(api_key=api_key)
 
-model = genai.GenerativeModel('gemini-2.5-flash')
+model = genai.GenerativeModel('gemini-2.0-flash')
 
 @app.route('/', methods=['GET'])
 def home():
